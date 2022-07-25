d58f35e47273361736c7804647d1b76cfd8dabb5
Darren Xu <darrenxu1994@gmail.com>
2022-07-25 21:17:21 +1000

    Added gsap

---
+++ js/animation.js
@@ -0,0 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#projects",
+    markers: true,
+    start: "bottom 80%", // when the top of the trigger hits the top of the viewport
+    toggleActions: "play reverse play reverse",
+    end: "bottom 50%", // end after scrolling 500px beyond the start
+  },
+});
+
+var tween = gsap.fromTo("#projects", { scale: 1 }, { scale: 0.9 });
+tl.add(tween);
